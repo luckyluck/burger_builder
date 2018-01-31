c0256922a1780f0ffdcbe0f229bb308e853e1ab3
Alexander <ketov.alex.v@yandex.kz>
2018-01-31 20:04:50 +0600

    Remove error boundaries.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styles from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
     state = {
@@ -48,14 +47,13 @@ class App extends Component {
             persons = (
                 <div>
                     {this.state.persons.map((person, index) => {
-                        return <ErrorBoundary key={person.id}>
-                            <Person
-                                click={() => this.deletePersonHandler(index)}
-                                name={person.name}
-                                age={person.age}
-                                changed={(event) => this.nameChangedHandler(event, person.id)}
-                            />
-                        </ErrorBoundary>
+                        return <Person
+                                    click={() => this.deletePersonHandler(index)}
+                                    name={person.name}
+                                    age={person.age}
+                                    changed={(event) => this.nameChangedHandler(event, person.id)}
+                                    key={person.id}
+                                />
                     })}
                 </div>
             );
